eb8a33e17bd7f3f1fb3e9168501350568b5a9144
chimalsky <ich.bin.abe@gmail.com>
2019-06-16 11:20:38 -0400

    trying eager loading

--- app/Http/Controllers/JusticeController.php
+++ app/Http/Controllers/JusticeController.php
@@ -109,6 +109,7 @@ class JusticeController extends Controller
     public function show(Conflict $conflict, Justice $justice, Request $request)
     {
         $type = $request->input('type');
+        $justice->getMetas();
         
         return view('justice.show', compact('justice', 'conflict', 'type'));
     }
@@ -122,6 +123,7 @@ class JusticeController extends Controller
     public function edit(Conflict $conflict, Justice $justice, Request $request)
     {
         $type = $request->input('type');
+        $justice->getMetas();
         
         return view('justice.edit', compact('justice', 'conflict', 'type'));
     }
