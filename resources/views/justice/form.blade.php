d58556f72b54e8df1af2f129ff66492f87200bb3
chimalsky <ich.bin.abe@gmail.com>
2019-06-16 11:09:24 -0400

    testing

--- resources/views/justice/form.blade.php
+++ resources/views/justice/form.blade.php
@@ -116,7 +116,17 @@
 
     @if ($justice->form)
         @foreach ($justice->form->items as $meta)
-            {{ 'items[' . $meta['name'] . ']' }}
+            
+            @if (!isset($meta['type']))
+                @include('components.form.englishBoolean', [
+                    'name' => 'items[' . $meta['name'] . ']',
+                    'label' => $meta['label'],
+                    'model' => $justice,
+                    'labels' => $meta['options']
+                ])
+            @elseif ($meta['type'] == 'dropdown')
+                @include('components.form.select', $meta)
+            @endif
         @endforeach
     @endif
 
