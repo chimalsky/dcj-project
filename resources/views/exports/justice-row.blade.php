3faba21d2e9699f924af55e18a66038964e598d5
Abraham Kim <gucci@140-182-101-136.ssl-vpn.indiana.edu>
2020-05-21 18:04:29 -0400

    All Dyadic Conflict rows should have id/locaiton info

--- resources/views/exports/justice-row.blade.php
+++ resources/views/exports/justice-row.blade.php
@@ -11,18 +11,24 @@
     </td>
     <td>{{ $dyad->dyad_id  }}</td>
     <td>
+        {{ $dyad->conflict->conflict_id }}
+
         @if (isset($justice))
-            {{ $justice->conflict->conflict_id }}
+             -- {{ $justice->conflict->conflict_id }}
         @endif 
     </td>
     <td>
+        {{ $dyad->conflict->old_conflict_id }}
+
         @if (isset($justice))
-            {{ $justice->conflict->old_conflict_id }}
+            -- {{ $justice->conflict->old_conflict_id }}
         @endif
     </td>
     <td>
+        {{ $dyad->conflict->location }}
+
         @if (isset($justice))
-            {{ $justice->conflict->location }}
+            -- {{ $justice->conflict->location }}
         @endif
     </td>
 
